5732dfdedc8ce4366c4f901c3f19f123134e4b35
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-11-10 12:00:07 +0600

    problem 6 complete

--- src/problem-6.ts
+++ src/problem-6.ts
@@ -1,18 +1,33 @@
 // problem-6
 //Define an interface Profile with properties name, age, and email. Create a function updateProfile that accepts an object of type Profile and an object of type Partial representing the updates. The function should return the updated profile.
+// interface Profile {
+//     name: string;
+//     age: number;
+//     email: string;
+// }
+// const myProfile: Profile = {
+//     name: "Alice",
+//     age: 25,
+//     email: "alice@example.com"
+// }
+
+// const updateProfile = (profile: Profile, updates: <retuntprofle>) => {
+//     return { ...profile, ...retuntprofle }
+// }
+
+// const outPut = updateProfile(myProfile, { age: 26 })
+
 interface Profile {
     name: string;
     age: number;
     email: string;
 }
-const myProfile: Profile = {
-    name: "Alice",
-    age: 25,
-    email: "alice@example.com"
+
+function updateProfile(profile: Profile, updates: Partial<Profile>): Profile {
+    return { ...profile, ...updates };
 }
 
-const updateProfile = (profile: Profile, updates: <myProfile>) => {
-    return { ...profile, updates }
-}
-
-const outPut = updateProfile(myProfile, { age: 26 })
+const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+// const myProfileUpdate = updateProfile(myProfile, { age: 26 });
+// console.log(myProfileUpdate);
+updateProfile(myProfile, { age: 26 })
---
+++ src/problem-8.ts
@@ -0,0 +1,33 @@
+// problem-8
+
+//Create a function validateKeys(obj: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. The function should return true if all of the specified keys exist in the object; otherwise, it should return false.
+
+{
+    //
+
+
+    // function validateKeys<T>(obj: T, keys: (keyof T)[]): boolean {
+    //     for (let key of keys) {
+    //         if (!(key in obj)) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    // // Sample Input
+    // const person = { name: "Alice", age: 25, email: "alice@example.com" };
+    // console.log(validateKeys(person, ["name", "age"])); 
+    // console.log(validateKeys(person, ["name", "phone"])); 
+
+
+
+
+
+
+
+
+
+
+    //
+}
