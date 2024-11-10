d5d5a22fcd63313b1c729ce1b22937cfdbb7e8cb
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-11-10 10:31:24 +0600

    problem 5 has done

---
+++ src/problem-four.ts
@@ -0,0 +1,2 @@
+// problem-4
+// Define a union type Circle and Rectangle, where each type has a unique shape property. Create a function calculateShapeArea that uses type guards to calculate the area based on the shape type.
